90ab4fcc7465b51eddc33bab86f1c862e8c848e8
Michael Bock <me@thought2.de>
2021-02-27 08:33:59 +0100

    Init

---
+++ examples/simple.ts
@@ -0,0 +1,10 @@
+import { add, sub, div, Vec2n } from "ts-vector-maths/vec2n";
+import { pipe } from "fp-ts/function";
+
+const result: Vec2n = pipe(
+  add([0.456, 4.34], [0.1, 2]),
+  _ => sub(_, [1, 3.1]),
+  _ => div(_, [2, 2])
+);
+
+console.log(result);
